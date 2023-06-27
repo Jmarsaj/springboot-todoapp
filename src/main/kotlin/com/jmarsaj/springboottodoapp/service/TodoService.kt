5ae6cf916395be1c8006cfef1b98dc5df5d67df0
Jmarsaj <80609678+Jmarsaj@users.noreply.github.com>
2023-06-27 12:14:43 -0700

    added service tag and updated schema creation

--- src/main/kotlin/com/jmarsaj/springboottodoapp/service/TodoService.kt
+++ src/main/kotlin/com/jmarsaj/springboottodoapp/service/TodoService.kt
@@ -3,7 +3,9 @@ package com.jmarsaj.springboottodoapp.service
 import com.jmarsaj.springboottodoapp.model.TodoEntity
 import com.jmarsaj.springboottodoapp.repository.TodoEntityRepository
 import org.springframework.data.domain.Sort
+import org.springframework.stereotype.Service
 
+@Service
 class TodoService(val todoRepository: TodoEntityRepository) {
 
     fun getList(): MutableList<TodoEntity>{
